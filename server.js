1d15f4ddc103bf35d22e341b06790416e8856718
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-04 12:39:13 -0500

    sending index and show data

---
+++ server.js
@@ -0,0 +1,9 @@
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
